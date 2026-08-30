a4016a82fc541c21e1a0ce076bd75582b0fd1fcb
TheHarmanCodes <168176533+TheHarmanCodes@users.noreply.github.com>
2026-08-30 20:14:11 +0530

    09: widget layout

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,33 +1,17 @@
 "use client"
-import { useVapi } from "@/modules/hooks/use-vapi"
-import { Button } from "@workspace/ui/components/button"
 
-export default function Page() {
-  const {
-    isSpeaking,
-    isConnecting,
-    isConnected,
-    transcript,
-    startCall,
-    endCall,
-  } = useVapi()
-  return (
-    <div className="mx-auto flex min-h-svh w-full max-w-md flex-col items-center justify-center">
-      <Button onClick={() => startCall()}>Start Call</Button>
+import { use } from "react"
+import { WidgetView } from "@/modules/widget/ui/views/widget-view"
 
-      <Button
-        onClick={() => endCall()}
-        variant="destructive"
-        className="mt-4 mb-4"
-      >
-        End Call
-      </Button>
-
-      <p>isConnected: {`${isConnected}`}</p>
-      <p>isConnecting: {`${isConnecting}`}</p>
-      <p>isSpeaking: {`${isSpeaking}`}</p>
-
-      <p>{JSON.stringify(transcript, null, 2)}</p>
-    </div>
-  )
+interface Props {
+  searchParams: Promise<{
+    organizationId: string
+  }>
 }
+
+const Page = ({ searchParams }: Props) => {
+  const { organizationId } = use(searchParams)
+  return <WidgetView organizationId={organizationId} />
+}
+
+export default Page
